4a7890bdbfa10947c51b3de339bba26328ab9af9
haeun jeon <janice@sookmyung.ac.kr>
2026-01-19 18:17:00 +0900

    12

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // 프론트엔드에서 '/api'로 시작하는 요청을 보내면
+      "/api": {
+        // 백엔드 서버(80번 포트)로 전달해줍니다.
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
